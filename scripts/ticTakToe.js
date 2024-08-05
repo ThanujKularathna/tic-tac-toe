1bc3471d6dbc0194dea2f7c2c444b128d53e9937
thanujakularathna119@gmail.com <thanujakularathna119@gmail.com>
2024-08-05 08:54:30 +0530

    finishing the project

--- scripts/ticTakToe.js
+++ scripts/ticTakToe.js
@@ -1,51 +1,140 @@
-const playerMove = 'x';
-const computerMove = 'o';
+const playerMove = "x";
+const computerMove = "o";
 
-let board = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
-];
-function insertPositionList(position) {
+let board = [ "", "", "",
+              "", "", "",
+              "", "", ""];
 
-//finding the row position of the board
-  let row = 0;
-  let column = 0;
+function getRandomPosition() {
+  return Math.floor(Math.random() * 9);
+}
 
-  if (position >= 0 && position <= 2) {
-    row = 0;
-  } else if (position >= 3 && position <= 5) {
-    row = 1;
-  } else {
-    row = 2;
+
+
+function whiteSapces() {
+  let freeSpaces = 9;
+
+  for (let i = 0; i < 9; i++){
+    if (board[i] !== "") {
+      freeSpaces--
+    }
   }
+  return freeSpaces
+}
 
-//finding the column position of the boad
-  let temp = position;
-  if (position >= 3) {
+function reset() {
+  for (let i = 0; i < board.length; i++){
+    board[i] = "";
+  }
+  document.querySelectorAll('.js-tic-tack').forEach((button) => {
+    button.innerHTML = "";
+  });
+  showBoard()
+  enableButtons();
+  winnerMsg("");
+
+}
+
+function computer() {
+  if (whiteSapces()>0) {
     while (true) {
-      temp -= 3;
-      if ([0, 1, 2].includes(temp)) {
-        column = temp;
+      let randomPosition = getRandomPosition();
+      if (board[randomPosition] === "") {
+        board[randomPosition] = computerMove;
+        showBoard();
         break;
       }
     }
-  } else {
-    column = position;
   }
+} 
 
-  board[row][column] = playerMove;
+function showBoard() {
+  board.forEach((move,position) => {
+    if (move !== "") {
+      document.querySelector(`.js-tick-tack-pos-${position}`)
+        .innerHTML = move;
+    }
+  })
 }
 
-function player() {
-  
-  document.querySelectorAll(".js-grid-button").forEach((button, position) => {
-    button.addEventListener("click", () => {
-      document.querySelector(`.js-button-${position}`).innerHTML = playerMove;
 
-      insertPositionList(position);
+
+function checkWinner() {
+  for (let i = 0; i <= 6; i += 3) {
+    if (board[i] === board[i + 1] && board[i + 1] === board[i + 2]) {
+      return board[i];
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
+      return board[i];
+    }
+  }
+
+  if (board[0] === board[4] && board[4] === board[8]) {
+    return board[0];
+  }
+  if (board[2] === board[4] && board[4] === board[6]) {
+    return board[2];
+  }
+
+  return ""
+  
+}
+
+function winnerMsg(msg) {
+  document.querySelector(".js-msg")
+  .innerHTML=msg
+}
+function enableButtons() {
+  document.querySelectorAll(".js-tic-tack").forEach((button) => {
+    button.disabled = false;
+  });
+}
+function disableButtons() {
+  document.querySelectorAll(".js-tic-tack").forEach((button) => {
+    button.disabled = true;
+  });
+}
+
+function displayWinner(winner) {
+  if (winner === "x") {
+    winnerMsg("you won");
+    disableButtons(); // Disable buttons after a winner is found
+  } else if (winner === "o") {
+    winnerMsg("you loose");
+    disableButtons(); // Disable buttons after a winner is found
+  } else {
+    winnerMsg("Tie");
+    disableButtons(); // Disable buttons after a tie
+  }
+}
+
+
+
+//creating reset button
+document.querySelector('.js-reset-btn').addEventListener("click", () => {
+  reset();
+});
+
+
+function player() {
+  document.querySelectorAll(".js-tic-tack").forEach((button, position) => {
+    button.addEventListener("click", () => {
+      if (board[position] === "") {
+        board[position] = playerMove;
+        showBoard();
+        computer(); // Call the computer's move after the player's move
+        const winner = checkWinner();
+        if (winner !== "") {
+          displayWinner(winner);
+        }
+      }
+      console.log(board);
     });
   });
 }
 
-player();
+player()
+
